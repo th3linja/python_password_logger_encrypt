7a034580eb89184c9e7390dfea90f6399fdabb0e
th3linja <th3linja@yahoo.com>
2019-03-19 23:58:44 -0700

    uploaded files

--- password_logger.py
+++ password_logger.py
@@ -1,16 +1,18 @@
 import string
 
 encrypted_file = open('encrypted_file', 'r')
+password_log = open('password_log', 'w')
+decrypter_file = open('decrypter_file', 'r')
+
 encrypted_text = [line.strip() for line in encrypted_file]
+decrypted_text = [line.strip() for line in decrypter_file]
 
 letter_up = list(string.ascii_uppercase)
 letter_low = list(string.ascii_lowercase)
 char_spec = list(string.punctuation)
 digit = list(string.digits)
 
-decrypter_file = open('decrypter_file', 'r')
-decrypted_text = [line.strip() for line in decrypter_file]
-
+password_for = input("Password for: ")
 password_to_encrypt = input("Password to be encrypted: ")
 encrypted_password = ''
 
@@ -20,6 +22,7 @@ for char in range(len(password_to_encrypt)):
     else:
         encrypted_password += encrypted_text[decrypted_text.index(password_to_encrypt[char])]
 
+password_log.write(password_for + ": " + encrypted_password)
 
 print(encrypted_password)
 encrypted_file.close()
