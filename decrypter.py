53bd7fe37008d609dabd188add745563e721b06f
th3linja <th3linja@yahoo.com>
2019-03-21 01:10:43 -0700

    uploaded files -

--- decrypter.py
+++ decrypter.py
@@ -1,8 +1,8 @@
-import string
 
 encrypted_file = open('encrypted_file', 'r')
 password_log = open('password_log', 'r')
 decrypter_file = open('decrypter_file', 'r')
+
 
 encrypted_text = [line.strip() for line in encrypted_file]
 decrypted_text = [line.strip() for line in decrypter_file]
@@ -15,22 +15,22 @@ for colon in range(len(password_text)):
     pass_for.append(password_text[colon][:password_text[colon].index(':')])
     pass_word.append(password_text[colon][password_text[colon].index(':')+2:])
 
+
 password_for = input("Password for: ")
 
 if password_for not in pass_for:
     print("You do not have a password saved for: " + password_for)
 
-password_to_decrypt = pass_word[pass_for.index(password_for) + 1]
+password_to_decrypt = pass_word[pass_for.index(password_for)]
 
 password = ''
 build_block = ''
 
 for code in range(len(password_to_decrypt)):
+    build_block += password_to_decrypt[code]
     if build_block in encrypted_text:
-        print(build_block)
         password += decrypted_text[encrypted_text.index(build_block)]
         build_block = ''
-    else:
-        build_block += password_to_decrypt[code]
+
 
 print(password)
--- password_logger.py
+++ password_logger.py
@@ -1,16 +1,9 @@
-import string
-
 encrypted_file = open('encrypted_file', 'r')
 password_log = open('password_log', 'a')
 decrypter_file = open('decrypter_file', 'r')
 
 encrypted_text = [line.strip() for line in encrypted_file]
 decrypted_text = [line.strip() for line in decrypter_file]
-
-letter_up = list(string.ascii_uppercase)
-letter_low = list(string.ascii_lowercase)
-char_spec = list(string.punctuation)
-digit = list(string.digits)
 
 password_for = input("Password for: ")
 password_to_encrypt = input("Password to be encrypted: ")
